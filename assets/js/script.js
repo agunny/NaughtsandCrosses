5e00b417e724ac256442de0379e6b0182598d38d
agunny <>
2023-09-09 11:35:21 +0000

    testing javascript by adding clickedOnce function and triggering

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
 let xElement = 'x';
 let oElement = 'circle';
-let boxes = document.getElementsByClassName('boxes');
+const boxesElement = document.querySelectorAll('[gameboxes]');
+let winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+boxesElement.forEach(boxes => {
+    boxes.addEventListener('click', clickedOnce, { once: true });
+});
+
+function clickedOnce(e) {
+    console.log('clicked once');
+}
